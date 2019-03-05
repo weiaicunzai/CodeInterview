9c590c4dd30994feef6eb5acd088748e4eabea28
weiaicunzai <372995411@qq.com>
2019-03-05 16:02:40 +0800

    git add

---
+++ 第三十一题_栈的压入，弹出序列/solution.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+
+        int index_push = 0;
+        int index_pop = 0;
+        if (pushV.empty() || popV.empty())  
+            return false;
+        
+        while(!pushV.empty() && !popV.empty()) 
+        {
+            //if(pushV[index_push] )
+            if(pushV)
+        }
+    }
+};
