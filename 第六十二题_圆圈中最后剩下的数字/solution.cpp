b856bc1592137d315cd4315cb958c2e0d18fcb05
weiaicunzai <372995411@qq.com>
2019-03-30 06:28:00 +0800

    第六十二题

---
+++ 第六十二题_圆圈中最后剩下的数字/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int LastRemaining_Solution(int n, int m)
+    {
+        if(n < 1 || m < 1)
+            return -1;
+        
+        int last = 0;
+        for(int i = 2; i <= n; i++)
+            last = (last + m) % i;
+        
+        return last;
+    }
+};
+
+int main()
+{
+
+}
